531db4e1e9b6a29d7ddb427ea26c3e993a235343
matt <matt@a48dd2e0-fd1b-294c-8aff-8c3a937dd1ff>
2011-05-06 16:44:33 +0000

    Began a HALTest project mostly just to make sure ByteDelimitedPacketFormatter works. Maybe we look at CTest for legit automated testing at some point?
    git-svn-id: https://shuttle.mil.ufl.edu/svn/subjugator_code/SubjuGator/trunk@34 a48dd2e0-fd1b-294c-8aff-8c3a937dd1ff

---
+++ HALTest/source/main.cpp
@@ -0,0 +1,40 @@
+#include "HAL/ByteDelimitedPacketFormatter.h"
+#include <boost/assign.hpp>
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace subjugator;
+using namespace boost;
+using namespace std;
+
+// just for ease of testing
+ChecksumValidationResults endsInQChecksum(ByteVec::const_iterator begin, ByteVec::const_iterator end) {
+	if (begin == end) // fail if packet has 0 length
+		return none;
+	if (end[-1] != 'Q') // fail if packet doesn't end in Q
+		return none;
+	return make_pair(begin, end-1); // otherwise, the data is the beginning of the packet to 1 before the end (the Q)
+}
+
+int main(int argc, char **argv) {
+	ByteDelimitedPacketFormatter packetformatter('!', endsInQChecksum); // valid packets are delimited with an ! and end in Q
+	string data = "!BAD!!BAD!!BAD!HelloQ!!WorldQ!Not a packetQ!BAD!!Split up"; // including half a packet at the end
+	ByteVec bytes(data.begin(), data.end());
+
+	vector<Packet> packets = packetformatter.parsePackets(bytes);
+
+	for (vector<Packet>::iterator i = packets.begin(); i != packets.end(); ++i) {
+		cout << string(i->begin(), i->end()) << endl;
+	}
+
+	data = " packetQ!!!"; // finish the split up packet, to verify that the packet formatter kept the previous half internally
+	bytes.assign(data.begin(), data.end());
+
+	packets = packetformatter.parsePackets(bytes);
+
+	for (vector<Packet>::iterator i = packets.begin(); i != packets.end(); ++i) {
+		cout << string(i->begin(), i->end()) << endl;
+	}
+}
+
