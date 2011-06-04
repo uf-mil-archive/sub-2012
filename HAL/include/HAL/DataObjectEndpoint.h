bca2431c74231af04b11cb021b88588c67ead6ec
Matt Thompson <matthewbot@gmail.com>
2011-06-04 00:40:00 -0400

    Added DataObjectEndpoint, which can read and write DataObjects to an Endpoint using a PacketFormatter and a DataObjectFormatter

---
+++ HAL/include/HAL/DataObjectEndpoint.h
@@ -0,0 +1,36 @@
+#ifndef HAL_DATAOBJECTENDPOINT_H
+#define HAL_DATAOBJECTENDPOINT_H
+
+#include "HAL/Endpoint.h"
+#include "HAL/DataObjectFormatter.h"
+#include "HAL/PacketFormatter.h"
+#include <boost/scoped_ptr.hpp>
+#include <memory>
+
+namespace subjugator {
+	class DataObjectEndpoint {
+		public:
+			DataObjectEndpoint(Endpoint *endpoint, DataObjectFormatter *dobjformat, PacketFormatter *packetformat);
+
+			typedef boost::function<void (std::auto_ptr<DataObject> &dobj)> ReadCallback;
+			typedef boost::function<void ()> StateChangeCallback;
+
+			void configureCallbacks(const ReadCallback &readcallback, const StateChangeCallback &statechangecallback);
+
+			void write(const DataObject &dobj);
+
+		private:
+			boost::scoped_ptr<Endpoint> endpoint;
+			boost::scoped_ptr<DataObjectFormatter> dobjformat;
+			boost::scoped_ptr<PacketFormatter> packetformat;
+
+			ReadCallback readcallback;
+			StateChangeCallback statechangecallback;
+
+			void endpointReadCallback(ByteVec::const_iterator begin, ByteVec::const_iterator end);
+			void endpointStateChangedCallback();
+	};
+}
+
+#endif
+
---
+++ HAL/source/DataObjectEndpoint.cpp
@@ -0,0 +1,37 @@
+#include "HAL/DataObjectEndpoint.h"
+#include <boost/bind.hpp>
+
+using namespace subjugator;
+using namespace boost;
+using namespace std;
+
+DataObjectEndpoint::DataObjectEndpoint(Endpoint *endpoint, DataObjectFormatter *dobjformat, PacketFormatter *packetformat)
+: endpoint(endpoint), dobjformat(dobjformat), packetformat(packetformat) {
+	endpoint->configureCallbacks(boost::bind(&DataObjectEndpoint::endpointReadCallback, this, _1, _2), boost::bind(&DataObjectEndpoint::endpointStateChangedCallback, this));
+}
+
+void DataObjectEndpoint::configureCallbacks(const ReadCallback &readcallback, const StateChangeCallback &statechangecallback) {
+	this->readcallback = readcallback;
+	this->statechangecallback = statechangecallback;
+}
+
+void DataObjectEndpoint::write(const DataObject &dobj) {
+	Packet packet = dobjformat->toPacket(dobj);
+	ByteVec bytes = packetformat->formatPacket(packet);
+
+	endpoint->write(bytes.begin(), bytes.end());
+}
+
+void DataObjectEndpoint::endpointReadCallback(ByteVec::const_iterator begin, ByteVec::const_iterator end) {
+	ByteVec bytes(begin, end); // TODO eliminate this unnecessary copy by making PacketFormatters operate on a pair of iterators
+	vector<Packet> packets = packetformat->parsePackets(bytes); // parse packets from the new data
+
+	for (vector<Packet>::iterator i = packets.begin(); i != packets.end(); ++i) { // for each packet
+		auto_ptr<DataObject> dobj(dobjformat->toDataObject(*i)); // attempt to convert it to a data object
+		if (dobj.get()) { // if we succeeded
+			if (readcallback)
+				readcallback(dobj); // call our read callback (which can take optionally take ownership of the dataobject, otherwise auto_ptr will delete it)
+		}
+	}
+}
+
