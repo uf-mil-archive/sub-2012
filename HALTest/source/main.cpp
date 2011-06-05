f37883ba26951195c7d6128a03a65b96df54d315
Matt Thompson <matthewbot@gmail.com>
2011-06-05 18:46:35 -0400

    New HALTest, now a simple way to do ramp motors, soon to be obsolete by the MotorCalibrate GUI

--- HALTest/source/main.cpp
+++ HALTest/source/main.cpp
@@ -15,6 +15,42 @@ using namespace subjugator;
 using namespace boost;
 using namespace std;
 
+static void rampUp(double start, double stop, double inc, double sleep, scoped_ptr<DataObjectEndpoint> &endpoint) {
+	double reference=start;
+
+	while (reference <= stop) {
+		endpoint->write(HeartBeat());
+		endpoint->write(SetReference(reference));
+		this_thread::sleep(posix_time::milliseconds(sleep*1000));
+		cout << "Ref " << reference << endl;
+		reference += inc;
+	}
+
+	endpoint->write(SetReference(stop));
+}
+
+static void rampDown(double start, double stop, double inc, double sleep, scoped_ptr<DataObjectEndpoint> &endpoint) {
+	double reference=start;
+
+	while (reference >= stop) {
+		endpoint->write(HeartBeat());
+		endpoint->write(SetReference(reference));
+		this_thread::sleep(posix_time::milliseconds(sleep*1000));
+		cout << "Ref " << reference << endl;
+		reference -= inc;
+	}
+
+	endpoint->write(SetReference(stop));
+}
+
+static void wait(double sleep, scoped_ptr<DataObjectEndpoint> &endpoint) {
+	int counts = (int)(sleep / .1);
+	for (int ctr=0; ctr<counts; ctr++) {
+		endpoint->write(HeartBeat());
+		this_thread::sleep(posix_time::milliseconds(100));
+	}
+}
+
 int main(int argc, char **argv) {
 	SubHAL hal;
 	scoped_ptr<DataObjectEndpoint> endpoint(hal.openDataObjectEndpoint(2, new MotorDriverDataObjectFormatter(2, 1, BRUSHEDOPEN), new Sub7EPacketFormatter()));
@@ -26,18 +62,15 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-	double reference;
-	do {
-		cout << "Reference: ";
-		cin >> reference;
-	} while (reference > 1 || reference < -1);
+	rampUp(0, 1, .01, .1, endpoint);
+	wait(2, endpoint);
+	rampDown(1, 0, .01, .1, endpoint);
+	wait(1, endpoint);
+	rampDown(0, -1, .01, .1, endpoint);
+	wait(2, endpoint);
+	rampUp(-1, 0, .01, .1, endpoint);
+	wait(1, endpoint);
 
-	endpoint->write(HeartBeat());
-	endpoint->write(SetReference(reference));
-	while (true) {
-		endpoint->write(HeartBeat());
-		this_thread::sleep(posix_time::milliseconds(1000));
-		cout << "Heartbeat" << endl;
-	}
+	endpoint->close();
 }
 
