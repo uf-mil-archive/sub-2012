6a4df13c9ada4091c8375c09a21aaadf8f8b84ee
Matt Thompson <matthewbot@gmail.com>
2011-06-09 00:16:11 -0400

    Made FindFFTW not print messages when successfully found

--- cmake/FindFFTW.cmake
+++ cmake/FindFFTW.cmake
@@ -1,8 +1,3 @@
-
-if (FFTW_INCLUDES AND FFTW_LIBRARIES)
-  set(FFTW_FIND_QUIETLY TRUE)
-endif (FFTW_INCLUDES AND FFTW_LIBRARIES)
-
 find_path(FFTW_INCLUDES
   NAMES
   fftw3.h
@@ -20,9 +15,6 @@ find_library(FFTWL_LIB NAMES fftw3l  PATHS $ENV{FFTWDIR} ${LIB_INSTALL_DIR})
 if(FFTWL_LIB)
 set(FFTW_LIBRARIES ${FFTW_LIBRARIES} ${FFTWL_LIB})
 endif()
-
-
-message(STATUS "FFTW ${FFTW_LIBRARIES}" )
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(FFTW DEFAULT_MSG
