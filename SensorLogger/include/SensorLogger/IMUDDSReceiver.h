c28414856218161a4b09b7795f7987e44b8e593e
Matt Thompson <matthewbot@gmail.com>
2011-06-17 14:33:54 -0400

    Easier than expected to template over DDS. Now we can just typedef DDSReceiver and make IMUDDSReceiver, DVLDDSReceiver, etc

--- SensorLogger/include/SensorLogger/IMUDDSReceiver.h
+++ SensorLogger/include/SensorLogger/IMUDDSReceiver.h
@@ -3,26 +3,12 @@
 
 #include "DDSMessages/IMUMessage.h"
 #include "DDSMessages/IMUMessageSupport.h"
+#include "SensorLogger/DDSReceiver.h"
 #include <ndds/ndds_cpp.h>
 #include <boost/function.hpp>
 
 namespace subjugator {
-	class IMUDDSReceiver : DDSDataReaderListener {
-		public:
-			typedef boost::function<void (const IMUMessage &message)> Callback;
-
-			IMUDDSReceiver(DDSDomainParticipant *participant, const Callback &callback);
-			~IMUDDSReceiver();
-
-		private:
-			DDSDomainParticipant *participant;
-			DDSTopic *topic;
-			IMUMessageDataReader *imureader;
-
-			Callback callback;
-
-			virtual void on_data_available(DDSDataReader *unused);
-	};
+	typedef DDSReceiver<IMUMessage, IMUMessageDataReader, IMUMessageTypeSupport, IMUMessageSeq> IMUDDSReceiver;
 }
 
 #endif
--- SensorLogger/source/main.cpp
+++ SensorLogger/source/main.cpp
@@ -1,9 +1,43 @@
+#include "SensorLogger/IMUDDSReceiver.h"
 #include <iostream>
+#include <boost/thread.hpp>
 
-//using namespace subjugator;
+using namespace subjugator;
+using namespace boost;
+using namespace boost::posix_time;
 using namespace std;
 
+void callback(const IMUMessage &message) {
+	cout << "Got message!";
+
+	cout << "Accels: ";
+	for (int i=0; i<3; i++)
+		cout << message.accel[i] << " ";
+	cout << endl;
+
+	cout << "Gyros: ";
+	for (int i=0; i<3; i++)
+		cout << message.gyro[i] << " ";
+	cout << endl;
+
+	cout << "Mags: ";
+	for (int i=0; i<3; i++)
+		cout << message.mag[i] << " ";
+	cout << endl;
+}
+
 int main(int argc, char **argv) {
-	cout << "Hello World" << endl;
+	DDSDomainParticipant *participant = DDSDomainParticipantFactory::get_instance()->create_participant(0, DDS_PARTICIPANT_QOS_DEFAULT, NULL, DDS_STATUS_MASK_NONE);
+	if (!participant)
+		throw runtime_error("Failed to create DDSDomainParticipant");
+
+	// TODO centralize all type registrations somewhere in DDSMessages
+	if (IMUMessageTypeSupport::register_type(participant, IMUMessageTypeSupport::get_type_name()) != DDS_RETCODE_OK)
+		throw runtime_error("Failed to register type");
+
+	IMUDDSReceiver imureceiver(participant, "IMU", callback);
+
+	while (true)
+		this_thread::sleep(seconds(1));
 }
 
