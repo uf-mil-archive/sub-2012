7be62ddad550958848413b33ca3e58969e244fe9
Devin Hughes <devin@example.com>
2011-06-25 20:29:37 -0400

    Working on the kalman, not finished yet

--- SubMain/include/SubMain/Workers/LPOSVSS/SubKalman.h
+++ SubMain/include/SubMain/Workers/LPOSVSS/SubKalman.h
@@ -25,6 +25,39 @@ namespace subjugator
 		{
 		}
 	};
+
+	class KalmanFilter
+	{
+	public:
+		typedef Matrix<double, 13, 1> Vector13d;	// x_hat
+		typedef Matrix<double, 1, 26> RowVector26d;	// helper vectors
+		typedef Matrix<double, 1, 27> RowVector27d;
+		typedef Matrix<double, 13, 13> Matrix13d;
+		typedef Matrix<double, 7, 7> Matrix7d;
+		//typedef Matrix<double, >
+	public:
+
+
+		//public Vector3d getP_Error() { return }
+
+	private:
+		boost::mutex lock;
+		boost::uint64_t prevTickCount;
+
+		RowVector27d ones2LXp1;
+		RowVector26d ones2LX;
+		double gravityMag;
+
+		int L;
+		Vector13d x_hat;
+
+		// Internally the units of the noise for the gyro and IMU are converted to match the units of the Kalman filter
+		// provided they are specified as documented in the matlab sensorCharacteristics file.
+		Vector4d q_hat;
+
+
+		Matrix13d P_Hat;
+	};
 }
 
 #endif /* SUBKALMAN_H_ */
