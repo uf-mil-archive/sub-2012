6941b1a1c82f61de8d315e82e83b8a8df5fa6e04
Matt Thompson <matthewbot@gmail.com>
2011-07-08 22:25:22 -0400

    Mission planner bugfixes and hydrophone tweaks

--- DDSCommanders/include/DDSCommanders/MissionPlannerDDSCommander.h
+++ DDSCommanders/include/DDSCommanders/MissionPlannerDDSCommander.h
@@ -4,6 +4,7 @@
 #include "DDSCommanders/LPOSVSSDDSReceiver.h"
 #include "DDSCommanders/PDStatusDDSReceiver.h"
 #include "DDSCommanders/FinderMessageListReceiver.h"
+#include "DDSCommanders/HydrophoneDDSReceiver.h"
 #include "SubMain/Workers/SubWorker.h"
 
 namespace subjugator {
@@ -15,15 +16,18 @@ namespace subjugator {
 			void receivedLPOSVSSInfo(const LPOSVSSMessage &lposvssinfo);
 			void receivedPDStatusInfo(const PDStatusMessage &pdstatusinfo);
 			void receivedFinderMessageListResult(const FinderMessageList &findermessages);
+			void receivedHydrophone(const HydrophoneMessage &hydrophonemessage);
 			
 			LPOSVSSDDSReceiver lposvssreceiver;
 			PDStatusDDSReceiver pdstatusreceiver;
 			FinderMessageListReceiver finderlistreceiver;
+			HydrophoneDDSReceiver hydrophonereceiver;
 
 			boost::weak_ptr<InputToken> lposvsscmdtoken;
 			boost::weak_ptr<InputToken> pdstatuscmdtoken;
 			boost::weak_ptr<InputToken> vision2dcmdtoken;
 			boost::weak_ptr<InputToken> vision3dcmdtoken;
+			boost::weak_ptr<InputToken> hydrophonecmdtoken;
 	};
 }
 
