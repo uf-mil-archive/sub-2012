d8a7765ffc8d66c314c7f6f455947c436087c6d8
Matt Thompson <matthewbot@gmail.com>
2011-07-13 11:10:36 -0400

    Fixes from tuesday night pool testing

--- DataObjects/source/Hydrophone/HydrophoneStart.cpp
+++ DataObjects/source/Hydrophone/HydrophoneStart.cpp
@@ -3,6 +3,6 @@
 using namespace subjugator;
 using namespace boost;
 
-static const uint8_t startbytes_array[] = { 0xAA, 0x55, 0xFF, 0x7F, 0x01, 0x80 };
+static const uint8_t startbytes_array[] = { 0xAA, 0x55, 0x17, 0x7C, 0xE7, 0x83 };
 const ByteVec HydrophoneStart::startbytes(startbytes_array, startbytes_array + sizeof(startbytes_array)); // C++0x actually fixes this kind of awkwardness
 
