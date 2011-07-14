7da41a6abf06cdb856fee7d82cfdad6c8cfbad8d
Devin <email@host.com>
2011-07-14 12:18:53 -0400

    Fixed forward camera quaternion

--- SubMain/include/SubMain/Workers/MissionPlanner/SubMissionPlannerWorker.h
+++ SubMain/include/SubMain/Workers/MissionPlanner/SubMissionPlannerWorker.h
@@ -59,7 +59,7 @@ namespace subjugator
 		void failState();
 		void allState();
 	private:
-		static const double MIN_DEPTH = 0.2; // m
+		static const double MIN_DEPTH = -30.0; // m
 		boost::mutex lock;
 
 		int wayNum;
