9d17a4a72a1db37397b9ab391e9817492c3491ae
Matt Thompson <matthewbot@gmail.com>
2012-01-10 14:13:27 -0500

    Module installing fixup

--- cmake/PythonUtils.cmake
+++ cmake/PythonUtils.cmake
@@ -1,4 +1,8 @@
-function(python_install_modules)
+function(python_install_module_dirs)
+	message("Python ${PYTHON_VERSION_STRING}")
+
 	set(path lib/python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}/site-packages)
-	install(FILES ${ARGN} DESTINATION ${path})
+	foreach(dir ${ARGN})
+		install(DIRECTORY ${dir}/ DESTINATION ${path} FILES_MATCHING PATTERN "*.py")
+	endforeach()
 endfunction()
