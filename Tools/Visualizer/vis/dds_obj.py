109f2b3b5211c60281faf8625b803ed7ae0431bf
Forrest Voight <forrest.voight@gmail.com>
2012-04-17 13:09:21 -0400

    Made dds_obj less callbacky

--- Tools/Visualizer/vis/dds_obj.py
+++ Tools/Visualizer/vis/dds_obj.py
@@ -7,29 +7,32 @@ import dds
 
 from . import util
 
-class ListenProtocol(object):
-    def __init__(self, topic, message_callback, die_callback):
+class Listener(object):
+    def __init__(self, topic, die_callback):
         self.topic = topic
-        self.message_callback, self.die_callback = message_callback, die_callback
+        self.die_callback = die_callback
         
         self.die_timer = glib.timeout_add_seconds(5, self._die)
+        self.last_message = None
     
-    def touch(self):
+    def get_last(self):
         glib.source_remove(self.die_timer)
         self.die_timer = glib.timeout_add_seconds(5, self._die)
         
         try:
-            self.message_callback(self.topic.take())
+            self.last_message = self.topic.take()
         except dds.Error, e:
             if e.message != 'no data':
                 raise
+        
+        return self.last_message
     
     def _die(self):
         del self.topic
         self.die_callback()
 
 class DataObject(object):
-    '''a magic object that has topics' last messages as attributes (None if a message has not been received yet)'''
+    '''a magic object that has topics' last messages as attributes (raises DataNotYetAvailableError if a message has not been received yet)'''
     
     # starts listening when a message is requested the first time and stops 5 seconds after it was last requested
     
@@ -38,16 +41,14 @@ class DataObject(object):
         self._message_library = dds.Library('libddsmessages_c.so')
         
         self._active_listeners = {}
-        self._topic_messages = {}
     
     def __getattr__(self, name):
         if name not in self._active_listeners:
             data_type = getattr(self._message_library, name + 'Message')
-            self._active_listeners[name] = ListenProtocol(self._dds.get_topic(name, data_type), lambda msg: self._topic_messages.update({name: msg}), lambda: (self._active_listeners.pop(name), self._topic_messages.pop(name, None)))
+            self._active_listeners[name] = Listener(self._dds.get_topic(name, data_type), lambda: self._active_listeners.pop(name))
         
-        self._active_listeners[name].touch()
-        
-        if name in self._topic_messages:
-            return self._topic_messages[name]
+        last = self._active_listeners[name].get_last()
+        if last is not None:
+            return last
         else:
             raise util.DataNotYetAvailableError(name)
