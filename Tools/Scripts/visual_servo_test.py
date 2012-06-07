3a6d41632535ac062d2f90e5e3097601df5f849d
Matt Thompson <matthewbot@gmail.com>
2012-06-07 15:24:03 -0400

    Adjustments to simple python scripts

--- Tools/Scripts/visual_servo_test.py
+++ Tools/Scripts/visual_servo_test.py
@@ -2,6 +2,7 @@ from subjugator import sched
 from subjugator import nav
 from subjugator import vision
 import math
+import dds
 
 @sched.Task('maintask')
 def maintask():
@@ -11,26 +12,39 @@ def maintask():
     sched.sleep(2)
     print 'Running'
 
-    oid = OBJECTID_BUOY_RED
+    oid = 6
     vision.set_ids([oid], 0)
-    nav.depth(1)
 
     while True:
         vision.wait()
-        obj = vision.get_object(oid)
-        if obj is None:
+        objs = vision.get_objects(oid)
+	if len(objs) == 0:
             print 'Object not seen'
             nav.stop()
             continue
+        objs.sort(key=lambda obj: float(obj['scale']))
+	obj = objs[0]
 
-        xvel = .5
-        ky = .01
-        kz = .01
+        ky = .0015
+        kz = .0015
+        yvel = ky*(int(obj['u'])-320) # Assumes u,v origin in top left
+        zvel = kz*(int(obj['v'])-240)
 
-        yvel = ky*(oid['u']-320) # Assumes u,v origin in top left
-        zvel = kz*(oid['v']-240)
+        scale = float(obj['scale'])
+        if scale < 2000:
+            xvel = .6
+        elif scale < 15000:
+            xvel = .3
+        else:
+            xvel = 0
+            if yvel < 0.001 and zvel < 0.001:
+                break
 
-        print xvel, yvel, zvel
+        print xvel, yvel, zvel, obj['scale']
         nav.vel(xvel, yvel, zvel)
+
+    print 'Bump!'
+    nav.fd(2, spd=.4)
+    nav.bk(4)
 
 sched.run()
