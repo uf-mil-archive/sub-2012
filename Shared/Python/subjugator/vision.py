d509fcc9ad8f632f2c31ff563b65cade7e3fb89e
Matt Thompson <matthewbot@gmail.com>
2012-06-10 00:29:56 -0400

    Added visual servo classes to vision.py

--- Shared/Python/subjugator/vision.py
+++ Shared/Python/subjugator/vision.py
@@ -22,6 +22,107 @@ def get_objects(objectid):
     topic = topics.get('Vision') # TODO, multiple camera. Need to change messages and QOS to work correctly here.
     try:
         msg = topic.read()
-        return [obj in map(json.loads, msg['messages']) if int(obj['objectID']) == objectid]
+        return [obj for obj in map(json.loads, msg['messages']) if int(obj['objectID']) == objectid]
     except dds.Error:
         return []
+
+def get_largest_object(objectid, field='scale'):
+    objs = vision.get_objects(objectid)
+    if len(objs) == 0:
+        return None
+    return max(objs, lambda obj: float(obj[field]))
+
+class VisualAlgorithm(object):
+    def __init__(self, objectid, cameraid):
+        self.objectid = objectid
+        self.cameraid = cameraid
+
+    def run(self):
+        set_ids([objectid], cameraid)
+        failctr = 0
+        while True:
+            vision.wait()
+            obj = get_largest_object(self.objectid, self.cameraid)
+            if obj is not None:
+                if update(obj):
+                    break
+            else:
+                nav.stop()
+                failctr += 1
+                if failctr > 5:
+                    return False
+        nav.stop()
+    def __call__(self):
+        return self.run()
+
+    def update(self):
+        raise NotImplementedError()
+
+class ForwardVisualAlgorithm(VisualAlgorithm):
+    def __init__(self, objectid, cameraid, fastvel, slowscale, slowvel, maxscale):
+        VisualAlgorithm.__init__(self, objectid, cameraid)
+        self.fastvel = fastvel
+        self.slowscale = slowscale
+        self.slowvel = slowvel
+        self.maxscale = maxscale
+
+    def _get_vel(self, obj):
+        scale = float(obj['scale'])
+        if scale < self.slowscale:
+            return self.fastvel
+        elif scale < self.maxscale:
+            return self.slowvel
+        else:
+            return 0
+
+class StrafeVisualServo(ForwardVisualAlgorithm):
+    def __init__(self, objectid, cameraid, fastvel, slowscale, slowvel, maxscale, ky, kz):
+        ForwardVisualAlgorithm.__init__(self, objectid, cameraid, fastvel, slowscale, slowvel, maxscale)
+        self.ky = ky
+        self.kz = kz
+
+    def update(self, obj):
+        xvel = self._get_vel(obj)
+        yvel = self.ky*(int(obj['u'])-320)
+        zvel = self.kz*(int(obj['v'])-240)
+
+        if xvel == 0 and yvel < 0.005 and zvel < 0.005:
+            return True
+
+        nav.vel(xvel, yvel, zvel)
+        return False
+
+class YawVisualServo(ForwardVisualAlgorithm):
+    def __init__(self, objectid, cameraid, kY, kz, fastvel, slowscale, slowvel, maxscale):
+        ForwardVisualAlgorithm.__init__(self, objectid, cameraid, fastvel, slowscale, slowvel, maxscale)
+        self.kY = kY
+        self.kz = kz
+
+    def update(obj):
+        xvel = self._get_vel(obj)
+        Yvel = self.kY*(int(obj['u'])-320)
+        zvel = self.kz*(int(obj['v'])-240)
+
+        if xvel == 0 and Yvel < 0.005 and zvel < 0.005:
+            return True
+
+        nav.vel(xvel, 0, zvel, Y=Yvel)
+        return False
+
+class BottomVisualServo(VisualAlgorithm):
+    def __init__(self, objectid, cameraid, kx, ky, kY):
+        VisualAlgorithm.__init__(self, objectid, cameraid)
+        self.kx = kx
+        self.ky = ky
+        self.kY = kY
+
+    def update(obj):
+        xvel = self.kX*(int(obj['u'])-320)
+        yvel = self.ky*(int(obj['v'])-240)
+        Yvel = self.kY*float(obj['angle'])
+
+        if Yvel < 0.005:
+            return True
+
+        nav.vel(xvel, yvel, Y=Yvel)
+        return False
--- Tools/Scripts/visual_servo_test.py
+++ Tools/Scripts/visual_servo_test.py
@@ -4,6 +4,16 @@ from subjugator import vision
 import math
 import dds
 
+servo = vision.StrafeVisualServo(objectid=6,
+                                 cameraid=1,
+                                 fastvel=.6,
+                                 slowscale=2000,
+                                 slowvel=.2,
+                                 maxscale=15000,
+                                 ky=.0015,
+                                 kz=.0015)
+
+
 @sched.Task('maintask')
 def maintask():
     print 'Waiting for nav setup'
@@ -12,39 +22,11 @@ def maintask():
     sched.sleep(2)
     print 'Running'
 
-    oid = 6
-    vision.set_ids([oid], 0)
-
-    while True:
-        vision.wait()
-        objs = vision.get_objects(oid)
-	if len(objs) == 0:
-            print 'Object not seen'
-            nav.stop()
-            continue
-        objs.sort(key=lambda obj: float(obj['scale']))
-	obj = objs[0]
-
-        ky = .0015
-        kz = .0015
-        yvel = ky*(int(obj['u'])-320) # Assumes u,v origin in top left
-        zvel = kz*(int(obj['v'])-240)
-
-        scale = float(obj['scale'])
-        if scale < 2000:
-            xvel = .6
-        elif scale < 15000:
-            xvel = .3
-        else:
-            xvel = 0
-            if yvel < 0.001 and zvel < 0.001:
-                break
-
-        print xvel, yvel, zvel, obj['scale']
-        nav.vel(xvel, yvel, zvel)
-
-    print 'Bump!'
-    nav.fd(2, spd=.4)
-    nav.bk(4)
+    if servo():
+        print 'bump!'
+        nav.fd(1)
+        nav.bk(4)
+    else:
+        print 'servo failed'
 
 sched.run()
