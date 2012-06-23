97a94f689424bc7d962306a7eb7f4d69deb25a2f
Forrest Voight <forrest.voight@gmail.com>
2012-06-23 11:47:48 -0400

    In HedgeFinder, get y coordinate from vertical bars and x from horizontal bar. works well when hedge is only partly visible

--- Mission/Vision/source/HedgeFinder.cpp
+++ Mission/Vision/source/HedgeFinder.cpp
@@ -24,33 +24,43 @@ vector<property_tree::ptree> HedgeFinder::find(IOImages* ioimages) {
 		erode(ioimages->dbg,ioimages->dbg,cv::Mat::ones(3,3,CV_8UC1));
 		dilate(ioimages->dbg,ioimages->dbg,cv::Mat::ones(7,7,CV_8UC1));
 
-		Blob blob(ioimages, 300, 10000, 2000);
+		Blob blob(ioimages, 300, 100000, 20000);
 		blob.drawResult(ioimages, objectName);
 
-		if(blob.data.size() == 0)
-			continue;
-
-		Point center(0, 0);
-		int min_width = INT_MAX;
-		int max_width = INT_MIN;
+		// find bottom bar. assumes that more than one matching blob is never detected
+		Point bottom_center;
+		bool found_bottom = false;
 		BOOST_FOREACH(const Blob::BlobData &data, blob.data) {
-			if(data.centroid.x < min_width) min_width = data.centroid.x;
-			if(data.centroid.x > max_width) max_width = data.centroid.x;
-			center.x += data.centroid.x;
-			center.y += data.centroid.y;
+			bool is_vertical = pow(sin(data.angle), 2) > pow(cos(data.angle), 2);
+			if(!is_vertical && 12 < data.aspect_ratio && data.aspect_ratio < 19) {
+				bottom_center = data.centroid;
+				found_bottom = true;
+			}
 		}
-		center.x /= blob.data.size();
-		center.y /= blob.data.size();
+		if(!found_bottom) continue;
 
-		int diff_width = max_width - min_width;
-		
-		circle(ioimages->res,center,10,Scalar(255,255,255),5);
+		// find y coordinate from maximum y centroid of vertical bars that are above the bottom bar
+		float center_y = -1000;
+		bool found_side = false;
+		BOOST_FOREACH(const Blob::BlobData &data, blob.data) {
+			bool is_vertical = pow(sin(data.angle), 2) > pow(cos(data.angle), 2);
+			if(is_vertical && 4 < data.aspect_ratio && data.aspect_ratio < 10 &&
+					data.centroid.y < bottom_center.y && data.centroid.y > center_y) {
+				center_y = data.centroid.y;
+				found_side = true;
+			}
+		}
+		if(!found_side) continue;
+
+		Point center(bottom_center.x, center_y);
+		float scale = bottom_center.y - center_y;
+		circle(ioimages->res,center,scale,Scalar(255,255,255),5);
 
 		// Prepare results
 		property_tree::ptree fResult;
 		fResult.put("objectName", objectName);
 		fResult.put_child("center", Point_to_ptree(center, ioimages->prcd));
-		fResult.put("scale", diff_width);
+		fResult.put("scale", scale);
 		resultVector.push_back(fResult);
 	}
 	return resultVector;
