9c427acce62db0805ee0ebef59eabe29b51c43cf
Forrest Voight <forrest.voight@gmail.com>
2012-07-08 12:16:20 -0400

    changed GrapesFinder to only find red areas that are within yellow areas

--- Mission/Vision/source/GrapesFinder.cpp
+++ Mission/Vision/source/GrapesFinder.cpp
@@ -30,67 +30,37 @@ vector<property_tree::ptree> GrapesFinder::find(IOImages* ioimages)
 		if(objectPath.size() != 1)
 			throw runtime_error("invalid grapes objectName");
 
-		// call to thresholder here
+		Thresholder::threshConfig(ioimages, config.get_child("thresh_yellow"));
+		dilate(ioimages->dbg,ioimages->dbg,cv::Mat::ones(5,5,CV_8UC1));
+		erode(ioimages->dbg,ioimages->dbg,cv::Mat::ones(7,7,CV_8UC1));
+		Contours contours(ioimages->dbg, 1000, 7000000, 1500000);
+		Mat yellow = ioimages->dbg.clone();
+
+		contours.drawResult(ioimages, objectName);
+
+		// mask gaps within yellow
+		Mat tempMask = Mat::zeros(ioimages->src.size(), CV_8UC1);
+		BOOST_FOREACH(const Contours::InnerContour &shape, contours.shapes)
+			drawContours(tempMask, shape.contour, 0, Scalar(255), CV_FILLED, 1, vector<Vec4i>(), 5);
+		dilate(tempMask, tempMask, cv::Mat::ones(5,5,CV_8UC1));
 		Thresholder::threshConfig(ioimages, config.get_child("thresh_red"));
-		//dilate(ioimages->dbg,ioimages->dbg,cv::Mat::ones(3,3,CV_8UC1));
-		//erode(ioimages->dbg,ioimages->dbg,cv::Mat::ones(4,4,CV_8UC1));
+		bitwise_and(ioimages->dbg, tempMask, ioimages->dbg); // use mask to only find red areas within holes in yellow
 
 		Blob blob(ioimages, 30, 1000000, 1000000);
 
 		// Draw result
 		blob.drawResult(ioimages, objectName);
 
+		ioimages->dbg |= yellow/2;
+
 		BOOST_FOREACH(const Blob::BlobData &b, blob.data) {
 			// Prepare results
 			property_tree::ptree fResult;
 			fResult.put("objectName", objectName);
 			fResult.put_child("center", Point_to_ptree(b.centroid, ioimages->prcd));
-			fResult.put("scale", b.area);
-			fResult.put("hue", b.hue);
+			fResult.put("scale", b.radius);
 			resultVector.push_back(fResult);
 		}
-
-/*
-		// call to specific member function here
-		Contours contours(500,7000000,1500000);
-		int result = contours.findContours(ioimages, true);
-		contours.sortBoxes();
-		contours.orientationError();
-
-		// Draw result
-		contours.drawResult(ioimages, objectName);
-		if(contours.shapes.size() == 0)
-			continue;
-
-		if(objectPath[2] == "box") {
-			// Prepare results
-			if(!result)
-				continue;
-			property_tree::ptree fResult;
-			fResult.put("objectName", objectName);
-			fResult.put_child("center", Point_to_ptree(contours.boxes[0].centroid, ioimages->prcd));
-			fResult.put("scale", contours.boxes[0].area);
-			fResult.put("angle", contours.boxes[0].orientationError);
-			resultVector.push_back(fResult);
-		} else if(objectPath[2] == "small") {
-			int index = contours.findSmallestShape();
-			property_tree::ptree fResult;
-			fResult.put("objectName", objectName);
-			fResult.put_child("center", Point_to_ptree(contours.shapes[index].centroid, ioimages->prcd));
-			//fResult.put("angle", contours.boxes[0].orientationError);
-			fResult.put("scale", contours.shapes[index].area);
-			resultVector.push_back(fResult);
-		} else if(objectPath[2] == "large") {
-			int index = contours.findLargestShape();
-			property_tree::ptree fResult;
-			fResult.put("objectName", objectName);
-			fResult.put_child("center", Point_to_ptree(contours.shapes[index].centroid, ioimages->prcd));
-			//fResult.put("angle", contours.boxes[0].orientationError);
-			fResult.put("scale", contours.shapes[index].area);
-			resultVector.push_back(fResult);
-		} else
-			throw runtime_error("unknown objectName in GrapesFinder::find: " + objectPath[2]);
-*/
 	}
 	return resultVector;
 }
