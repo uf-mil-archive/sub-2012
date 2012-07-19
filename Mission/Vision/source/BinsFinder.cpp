d7146d8ab044f97338d810eca46becf14503df39
Forrest Voight <forrest.voight@gmail.com>
2012-07-19 12:51:06 -0400

    enabled bins gaussian blur

--- Mission/Vision/source/BinsFinder.cpp
+++ Mission/Vision/source/BinsFinder.cpp
@@ -20,7 +20,7 @@ vector<property_tree::ptree> BinsFinder::find(IOImages* ioimages) {
 	Normalizer::norm(ioimages);
 	
 	// blur the image to remove noise
-	//GaussianBlur(ioimages->prcd,ioimages->prcd,Size(3,3),10,15,BORDER_DEFAULT);
+	GaussianBlur(ioimages->prcd,ioimages->prcd,Size(0,0),1.3);
 	ioimages->processColorSpaces();
 
 	vector<property_tree::ptree> resultVector;
